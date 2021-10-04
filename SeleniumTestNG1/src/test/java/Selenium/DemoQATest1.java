e3b2fc5595bcf83375ae6a104622e1b88621e4b2
zach-gilbert <zacgil7065@gmail.com>
2021-10-04 11:09:26 -0400

    First commit

---
+++ SeleniumTestNG1/src/test/java/Selenium/DemoQATest1.java
@@ -0,0 +1,55 @@
+package Selenium;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class DemoQATest1 {
+	private WebDriver driver;
+
+	@BeforeTest
+	public void setupTest() {
+		System.setProperty("webdriver.chrome.driver", "src\\test\\resources\\chromedriver.exe");
+		driver = new ChromeDriver();
+	}
+
+	@Test
+	public void verifyHomepageTitle() {
+		/*
+		 * Checks the homepage title
+		 * Expected: pass
+		 */
+		
+		String expectedTitle = "ToolsQA";
+		String foundTitle;
+		
+		driver.get("https://demoqa.com");
+		foundTitle = driver.getTitle();
+		
+		Assert.assertEquals(foundTitle, expectedTitle);
+	}
+	
+	@Test
+	public void verifyHomepageTitle2() {
+		/*
+		 * Checks the homepage title
+		 * Expected: fail
+		 */
+		
+		String expectedTitle = "QaStuff";
+		String foundTitle;
+		
+		driver.get("https://demoqa.com");
+		foundTitle = driver.getTitle();
+		
+		Assert.assertEquals(foundTitle, expectedTitle);
+	}
+	
+	@AfterTest
+	public void tearDown() {
+		driver.close();
+	}
+}
